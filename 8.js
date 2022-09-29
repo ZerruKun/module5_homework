58df2e96f9f32380c618d8b87bf02392bea9434f
Tabalin Nikolay <Zerru_Kun@yahoo.com>
2022-09-30 00:21:18 +0300

    Ex 8.

---
+++ 8.js
@@ -0,0 +1,11 @@
+let fruits = new Map([
+    ["apple", "green"],
+    ["strawberry", "red"],
+    ["blueberry", "blue"]
+]);
+
+for (let el of fruits.entries()) {
+    arr = [...el];
+    console.log(`Ключ - ${arr[0]}, значение - ${arr[1]}`);
+}
+
